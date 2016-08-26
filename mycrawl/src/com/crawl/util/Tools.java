4fc982219d3bbd2662dcbd05062fdd421c1a8f8a
JARVISTELLYES <1151770629@qq.com>
2016-08-26 08:35:16 +0800

    delete fork  it is new

---
+++ mycrawl/src/com/crawl/util/Tools.java
@@ -0,0 +1,34 @@
+package com.crawl.util;
+
+import java.io.File;
+
+/**
+ * Created by CUI on 2016/8/25.
+ * Student
+ */
+public class Tools {
+     public  static  boolean deleteFiles() {
+        String reg = ".*ilqs.*";
+        int i = 0;
+        //第一步  获得当前项目的路径
+        String path = System.getProperty("user.dir");
+        System.out.println(path);
+        File file = new File(path);
+        File[] tempList = file.listFiles();
+        for (File file1 : tempList) {
+            if (file1.isFile()) {
+                {
+                    if (file1.toString().matches(reg)) {
+                        System.out.println("Delete:" + file1);
+                        file1.delete();
+                        i++;
+                    }
+                }
+            }
+        }
+        System.out.println("一共删除了" + i + "个文件");
+        return true;
+    }
+
+
+}
